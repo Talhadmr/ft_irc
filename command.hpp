290528d7098c02ad6f247495d14af7f6cdf11b29
Baran <baranguzel2000@gmail.com>
2024-01-01 21:33:27 +0300

    is progress

--- command.hpp
+++ command.hpp
@@ -4,7 +4,7 @@
 
 # define RPL_PONG(user_id, token) (user_id + " PONG " + token + "\r\n")
 # define user_id(nickname, username) (":" + nickname + "!" + username + "@localhost" + "\r\n")
-# define RPL_WHOREPLY(nickname, channel, user, host, server, nick, status, hopcount, realname) "352 " + nickname + " " + channel + " " + user + " " + host + " " + server + " " + nick + " " + status + " " + hopcount + " :" + realname + "\r\n"
+# define RPL_WHOREPLY(nickname, channel, user, host, server, nick, status, hopcount, realname) "352 " + nickname + " " + channel + " " + user + " " + host + " " + server + " " + nick + " " + status + " " + hopcount + " : " + realname + "\r\n"
 # define RPL_ENDOFWHO(nickname, channel) "315 " + nickname + " " + channel + " :End of /WHO list" + "\r\n"
 # define ERR_NEEDMOREPARAMS(cmd) (std::string("461 ") + cmd + " :Not enough parameter" + "\r\n")
 # define ERR_NONICKNAMEGIVEN() ("431 :No nickname given\r\n")
